75d6a939d5b555d8e0aff8903d4cfebf06bd785b
baselelsrogy <baselhany88@gmail.com>
2025-04-03 03:00:03 +0200

    'add slug url'

--- job/models.py
+++ job/models.py
@@ -1,4 +1,5 @@
 from django.db import models 
+from django.utils.text import slugify
 
 JOB_TYPE = (
     ('Full Time','Full Time'),
@@ -22,7 +23,12 @@ class Job(models.Model): # table
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     image = models.ImageField(upload_to=image_upload)
+    slug = models.SlugField(null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        # logic
+        self.slug = slugify(self.title)
+        super(Job, self).save(*args, **kwargs)
 
 
     def __str__(self):
